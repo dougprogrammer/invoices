0cb671415cfdd828a5345d8fa0b158467edfaf8b
Douglas Martins Santos <douglassantos@cvc.com.br>
2017-10-30 18:30:52 -0200

    modulo two oday

--- features/pages/pages.rb
+++ features/pages/pages.rb
@@ -3,12 +3,12 @@ class LoginPage < SitePrism::Page
 	set_url '/login'
 	element :email, '#email'
 	element :password, 'input[type=password]'
-	element :sign_in, 'button[id*=password]'
+	element :sign_in, 'button[id*=btnLogin]'
 	element :alert_error, '#login=errors'
 
 	def do_login(user_login,password_login)
-		self.email.set user_login['Email']		  
-		self.password.set password_login['Senha']
+		self.email.set user_login		  
+		self.password.set password_login
 		self.sign_in.click
 	end
 	
@@ -41,7 +41,7 @@ class CustomerPage < SitePrism::Page
 	element :gender_m, 'input[name=radio-m]'
 	element :gender_f, 'input[name=radio-f]'
 	element :type, 'select[id=type-customer]'
-	element :note, 'texteare[name=note]'
+	element :note, 'textarea[name=note]'
 	element :info, 'input[type=checkbox]'
 	element :submit, 'button[id*=submit]'
 
@@ -54,19 +54,20 @@ class CustomerPage < SitePrism::Page
 
 	def save(customer)
 		self.new_customer.click
-		self.name.set @customer['Nome']
-		self.phone.set @customer['Celular']
-		self.email.set @customer['Email']
-		self.gender_m.click if @customer['Sexo'] == 'M'
-		self.gender_f.click if @customer['Sexo'] == 'F'
-		self.type.find('option', text: @customer['Tipo']).select_option
+		self.name.set customer['Nome']
+		self.phone.set customer['Celular']
+		self.email.set customer['Email']
+		self.gender_m.click if customer['Sexo'] == 'M'
+		self.gender_f.click if customer['Sexo'] == 'F'
+		self.type.find('option', text: customer['Tipo']).select_option
 		#choose option randon
 		#self.type.all('option').sample.select_option
 		#self.gender_f.click
-		self.note.set @customer['Obs']
+		self.note.set customer['Obs']
 		#My test this one our zero	
-		self.info.click if @customer['info'] == 'Sim'
+		self.info.click if customer['info'] == 'Sim'
 		self.submit.click
+		sleep 5
 	end
 
 	def search(email)
